6f77b54654be081eff60a40b932758e724c8b5b5
fjphysics <fjphysics@users.noreply.github.com>
2019-01-08 14:41:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert("dfsafhk");
+window.onload=function(){    
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    var imageData = context.createImageData(500, 500);
+    var pixels = imageData.data;
+
+    var numPixels = imageData.width * imageData.height;
+
+    for (var i = 0; i < numPixels; i++) {
+        pixels[i * 4] = 255; // Red 
+        pixels[i * 4 + 1] = 120; // Green 
+        pixels[i * 4 + 2] = 111; // Blue 
+        pixels[i * 4 + 3] = 255; // Alpha 
+    };
+    context.putImageData(imageData, 0, 0);  
+
+};
+
+document.getElementById("ranColor").addEventListener("change",changeColor);
+
+function changeColor(){
+
+  alert(document.getElementById("ranColor").value);  
+}
